a1a896e30972b179f2e27137d7460165d0b8192a
odabyskov <52351080+odabyskov@users.noreply.github.com>
2020-01-15 09:58:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -20,104 +20,41 @@ struct enemy_t ball; // We make it global to be able to use timing
 struct spaceship_t ship; // We initialize the spaceship as a global variable
 struct spaceshipBullet_t bullet; // We initialize the spaceship bullet as a global variable
 
-//uint32_t tid;
-/*
-while(1){
- //tid = counter.time;
- if (counter.time == 20){
-    printf("G\n");
-    counter.time=0;
- }
-}
-*/
-
-
-
 //struct spaceship_t ship;
 initSpaceship(&ship);
 drawSpaceship(&ship);
 initSpaceshipBullet(&bullet);
 
 char temp;
-int time;
 
 while(1){
 
+    //mainmenu();
     time = counter.time;
 
     if (uart_get_count() >= 1){
 
         temp = uart_get_char();
-       // updateSpaceshipPosition(&ship,temp);
-       // updateSpaceshipBulletPosition(&bullet, &ship,temp);
+        updateSpaceshipPosition(&ship, temp);
+        if (bullet.drawBullet == 0 ){
+        updateSpaceshipBulletPosition(&bullet, &ship, temp);
+        }
         uart_clear();
     }
 
     drawSpaceship(&ship);
+    drawSpaceshipBullet(&bullet);
 
     while (time > counter.time - 10){}
     if (time == counter.time - 10){
         updateSpaceshipBulletPosition(&bullet, &ship, 1);
-        updateSpaceshipPosition(&ship,1);
-
+        updateSpaceshipPosition(&ship, 1);
 
         drawSpaceshipBullet(&bullet);
+
+
     }
 }
-/*
-    if (counter.time >= 1){
-        drawSpaceship(&ship);
-        updateSpaceshipPosition(&ship);
-
-        if (counter.time == 10){
-            updateSpaceshipBulletPosition(&bullet, &ship);
-            drawSpaceshipBullet(&bullet);
-
-            counter.time=0;
-        }
-    }
-    */
-/*
-    if (counter.time == 10){
-        updateSpaceshipBulletPosition(&bullet, &ship);
-        drawSpaceshipBullet(&bullet);
-
-        counter.time=0;
-    }
-
-
-}
-*/
-
-
-//drawSpaceship(&ship);
-
-/*
-while(1){
-    drawSpaceship(&ship);
-    updateSpaceshipPosition(&ship);
-
-    drawSpaceshipBullet(&bullet);
-    if (bullet.timeAtPosition>200){
-        updateSpaceshipBulletPosition(&bullet);
-        drawSpaceshipBullet(&bullet);
-    }
-
-
-}
-*/
-/*
-initEnemy(&ball);
-drawEnemy(&ball);
-
-while(1){
-    drawEnemy(&ball);
-    if (ball.timeAtPosition>200){
-        updateEnemyPosition(&ball);
-        drawEnemy(&ball);
-    }
-}
-*/
 
 while(1){}
 }
