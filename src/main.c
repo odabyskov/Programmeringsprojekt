e454fd7dd380a86acce0a3d73eba572889492157
marcmorup <59912771+marcmorup@users.noreply.github.com>
2020-01-16 13:29:05 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -68,14 +68,14 @@ iter++;
 Boss key screen
 */
 if (temp == 'b'){
-    clrscr();
-    printf("Microsoft [Version 10.0.18362.535]");
+clrscr();
+printf("Microsoft [Version 10.0.18362.535]");
 printf("(c) 2019 Microsoft Corporation. Alle rettigheder forbeholdes");
 printf(" ");
 printf(" ");
 printf("C:/Users/LookBusy>");
-    clrscr();
-    while(uart_get_count() < 1){} 
+clrscr();
+while(uart_get_count() < 1){} 
 }  
 
 /*
