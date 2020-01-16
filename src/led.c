87fa95a60321853618e00163539838bf42fab3e3
marcmorup <59912771+marcmorup@users.noreply.github.com>
2020-01-16 13:22:01 +0100

    Update led.c

--- src/led.c
+++ src/led.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 
-
+//function that initializes the the LEDS
 void initializeLED(){
 
 RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
@@ -18,61 +18,56 @@ GPIOA->OTYPER |= (0x0000 << (9)); // Set output type register (0x00 - Push pull)
 GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
 GPIOA->MODER |= (0x00000001 << (9 * 2)); // Set mode register (0x01 - Output)
 
-// sluk for den
+// Turn off the led by turning on the first bit (or-operation)
 GPIOA->ODR |= (0x0001 << 9);
-//
+
 
 RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
 
 // Set pin PB4 to output
-GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
-GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); // set speed register (0x02 - 2 MHz)
-GPIOB->OTYPER &= ~(0x0001 << (4)); // Clear output type register
-GPIOB->OTYPER |= (0x0000 << (4)); // Set output type register (0x00 - Push pull)
-GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
-GPIOB->MODER |= (0x00000001 << (4 * 2)); // Set mode register (0x01 - Output)
-
-// sluk for den
+GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); 
+GPIOB->OSPEEDR |= (0x00000002 << (4 * 2)); 
+GPIOB->OTYPER &= ~(0x0001 << (4)); 
+GPIOB->OTYPER |= (0x0000 << (4)); 
+GPIOB->MODER &= ~(0x00000003 << (4 * 2)); 
+GPIOB->MODER |= (0x00000001 << (4 * 2)); 
+    
+// Turning off the led
 GPIOB->ODR |= (0x0001 << 4);
 //
-
 
 RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
 
 // Set pin PC7 to output
-GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
-GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); // set speed register (0x02 - 2 MHz)
-GPIOC->OTYPER &= ~(0x0001 << (7)); // Clear output type register
-GPIOC->OTYPER |= (0x0000 << (7)); // Set output type register (0x00 - Push pull)
-GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
-GPIOC->MODER |= (0x00000001 << (7 * 2)); // Set mode register (0x01 - Output)
+GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); 
+GPIOC->OSPEEDR |= (0x00000002 << (7 * 2)); 
+GPIOC->OTYPER &= ~(0x0001 << (7)); 
+GPIOC->OTYPER |= (0x0000 << (7)); 
+GPIOC->MODER &= ~(0x00000003 << (7 * 2)); 
+GPIOC->MODER |= (0x00000001 << (7 * 2)); 
 
-// sluk for den
+// Turn off the led
 GPIOC->ODR |= (0x0001 << 7);
-//
-
 }
 
+//function that take an integer and sets the LEDs accordingly
 void setLED(uint8_t ledhits){
 
-    if (ledhits == 0) {
-        GPIOC->ODR &= ~(0x0001 << 7);
+//if
+    if (ledhits == 1){                      //one hit turns on yellow (red and green)
+        GPIOB->ODR &= ~(0x0001 << 4);       //turn on red by turning off the first bit(and-operation with inverse of one)
+        GPIOC->ODR &= ~(0x0001 << 7);       //turn on green
     }
-    else if (ledhits == 1){
-        GPIOB->ODR &= ~(0x0001 << 4);
-        GPIOC->ODR &= ~(0x0001 << 7);
+    else if (ledhits == 2){                 //two hits turns on red
+        GPIOB->ODR &= ~(0x0001 << 4);       //turn on red
     }
-    else if (ledhits == 2){
-        GPIOB->ODR &= ~(0x0001 << 4);
+    else if (ledhits == 3) {                //three hits and you are dead. white light (red, green and blue)
+        GPIOB->ODR &= ~(0x0001 << 4);       //turn on red
+        GPIOC->ODR &= ~(0x0001 << 7);       //turn on green
+        GPIOA->ODR &= ~(0x0001 << 9);       //turn on blue
     }
-    else if (ledhits == 3) {
-        GPIOB->ODR &= ~(0x0001 << 4);
-        GPIOC->ODR &= ~(0x0001 << 7);
-        GPIOA->ODR &= ~(0x0001 << 9);
-    }
-
-    else{
-        GPIOB->ODR &= ~(0x0001 << 4);
+    else {
+    GPIOC->ODR &= ~(0x0001 << 7);           //zero hits equals green. green is default
     }
 }
 
