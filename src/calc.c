cb8ec3527f70292958718d3085c3153a83573902
Patrick <59878218+PatrickStrand@users.noreply.github.com>
2020-01-16 09:02:28 +0100

    Update calc.c

--- src/calc.c
+++ src/calc.c
@@ -116,29 +116,27 @@ void updateSpaceshipBulletPosition(struct spaceshipBullet_t *bullet, struct spac
 This function updates the bullet's position.
 The bullet moves vertically downwards from where it was fired.
 */
-void updateEnemyBulletPosition(struct enemyBullet_t *bullet, struct enemy1_t *enemy){
+void updateEnemyBulletPosition(struct enemyBullet_t *enemyBullet, struct enemy1_t *enemy){
 
-    if ((*enemy).shoot == 1 << FIX14_SHIFT && (*bullet).drawBullet == 0 << FIX14_SHIFT){
+    if ( (*enemyBullet).drawBullet == 0 << FIX14_SHIFT){
 
-        bullet->posX = (*enemy).posX; // the x-position is set to the middle of the spaceship
-        bullet->posY = (*enemy).posY + (2 << FIX14_SHIFT); // the y-position is set to the top of the spaceship
+        enemyBullet->posX = (*enemy).posX; // the x-position is set to the middle of the spaceship
+        enemyBullet->posY = (*enemy).posY + (2 << FIX14_SHIFT); // the y-position is set to the top of the spaceship
+        enemyBullet->drawBullet = convertTo1814(1);
 
-        bullet->drawBullet = convertTo1814(1);
-
-    } else if ((*bullet).drawBullet >= (1 << FIX14_SHIFT)){
+    } else if ( (*enemyBullet).drawBullet >= (1 << FIX14_SHIFT)){
 
         // We set the current positions to previous position
-        bullet->prevPosX=(*bullet).posX;
-        bullet->prevPosY=(*bullet).posY;
+        enemyBullet->prevPosX=(*enemyBullet).posX;
+        enemyBullet->prevPosY=(*enemyBullet).posY;
 
         // We update the current position
-        bullet->posX = (*bullet).posX; // the x-position is set to the middle of the spaceship
-        bullet->posY = (*bullet).posY + convertTo1814(1); // the y-position is set to the position above the previous position
+        enemyBullet->posX = (*enemyBullet).posX; // the x-position is set to the middle of the enemy
+        enemyBullet->posY = (*enemyBullet).posY + convertTo1814(1); // the y-position is set to the position below the previous position
 
     }
 
 }
-
 /*
 This function updates the position of enemy1
 */
